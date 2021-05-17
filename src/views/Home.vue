77a7fa03fe1c25ae1ea2104560090d916c955b30
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-05-17 13:40:58 +0200

    fetch local json file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
     <h1>Start page - Home</h1>
+    <p></p>
   </div>
 </template>
+
 <script>
+  export default {
+    name: 'Home',
 
-
-export default {
-  name: "Home",
-};
+    created() {
+      this.fetchLocalData()
+    },
+    methods: {
+      fetchLocalData() {
+        fetch('products.json')
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+          })
+      }
+    }
+  }
 </script>
