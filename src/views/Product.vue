50b33c753be157e1f61e68d76d646ec3ee62fe3b
alonakirichenkoivanovna <alena1412199217@gmail.com>
2021-05-17 16:37:21 +0200

    Commit Alona 16:37

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <p>products detail</p>
+    <p>{{ oneObject }}</p>
+
     <b-card
       title="Card Title"
       img-src="https://picsum.photos/600/300/?image=25"
       img-alt="Image"
-      img-top
       tag="article"
       style="max-width: 20rem;"
-      class="mb-2"
+      id="details"
     >
       <b-card-text>
         Some quick example text to build on the card title and make up the bulk
@@ -23,21 +24,40 @@
 <script>
   export default {
     data() {
-      return {}
+      return {
+        products: [],
+        productID: 'id0003',
+        oneObject: ''
+      }
     },
     created() {
       this.fetchLocalData()
+      this.pickRightObject()
     },
     methods: {
       fetchLocalData() {
         fetch('products.json')
           .then((response) => response.json())
           .then((data) => {
-            console.log(data)
+            this.products = data.products
+            /*  console.log('hi', data.products.id) */
+            console.log('hi ffffffff', this.products)
           })
+      },
+      pickRightObject() {
+        for (let i = 0; i < this.products.length; i++) {
+          if (this.products[i].id === 'id0003') {
+            this.oneObject = this.products
+          }
+        }
+        console.log('AAAAAAAAAA', this.products.id)
       }
     }
   }
 </script>
 
-<style></style>
+<style>
+  p {
+    color: pink;
+  }
+</style>
