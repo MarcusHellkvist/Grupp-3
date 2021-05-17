04a896180ebbd908dc68c9fdf8ef332188f6499f
Maco <hellqvistmarcus@gmail.com>
2021-05-17 14:13:13 +0200

    added navigation structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Homepage</router-link> |
-      <router-link to="/product">Product</router-link> |
-      <router-link to="/cart">Cart</router-link> |
-      <router-link to="/checkout">Checkout</router-link> |
+  <b-container>
+    <div id="app">
+      <b-row id="nav">
+        <b-col>
+          <b-navbar toggleable="sm" type="light" variant="light">
+            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+            <b-navbar-brand>e-handel</b-navbar-brand>
+            <b-collapse id="nav-text-collapse" is-nav>
+              <b-navbar-nav>
+                <b-nav-link
+                  ><router-link to="/category/1"
+                    >Category 1</router-link
+                  ></b-nav-link
+                >
+                <b-nav-link
+                  ><router-link to="/category/2"
+                    >Category 2</router-link
+                  ></b-nav-link
+                >
+                <b-nav-link
+                  ><router-link to="/category/3"
+                    >Category 3</router-link
+                  ></b-nav-link
+                >
+              </b-navbar-nav>
+            </b-collapse>
+          </b-navbar>
+          <router-link to="/">Homepage</router-link> |
+          <router-link to="/product">Product</router-link> |
+          <router-link to="/cart">Cart</router-link> |
+          <router-link to="/checkout">Checkout</router-link> |
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <router-view />
+        </b-col>
+      </b-row>
     </div>
-    <router-view />
-  </div>
+  </b-container>
 </template>
 
 <style>
@@ -26,6 +57,7 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  margin: 10px;
 }
 
 #nav a.router-link-exact-active {
