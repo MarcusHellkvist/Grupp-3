f991b906f84cc74b70f6a1a659246e305c6629ee
atlalbasha <atlal.basha@gmail.com>
2021-05-17 13:24:21 +0200

    Bootstap@4 & SCSS added

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+// bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// sass
+import "./main.scss";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Checkout sida Atlal</h1>
+  <div>
+    <h1>Checkout</h1>
+    <b-form-input
+      id="input-none"
+      :state="null"
+      placeholder="No validation"
+    ></b-form-input>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
