60fdc6fe844ccfc5393a9c8547fd716cdebf4925
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-05-17 12:10:57 +0200

    Cleaned up start page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Grupp 3 new!!!!" />
-
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Grupp 3 new!!!!" />
-
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Grupp 3 new!!!!" />
+    <h1>Start page - Home</h1>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
