78eb580bd112c1f735455952f7bd407d43498c9f
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-05-17 14:09:51 +0200

    added our products to store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
     <h1>Start page - Home</h1>
-    <p></p>
+    <ol v-if="this.$store.state.products">
+      <li
+        v-for="product in this.$store.state.product.products"
+        :key="product.id"
+      >
+        {{ product.name }}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Home',
-
+    data() {
+      return {
+        product: null
+      }
+    },
     created() {
       this.fetchLocalData()
     },
@@ -17,7 +28,7 @@
         fetch('products.json')
           .then((response) => response.json())
           .then((data) => {
-            console.log(data)
+            this.$store.state.product = data
           })
       }
     }
