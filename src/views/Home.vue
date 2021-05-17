cf2262ad96dbe5976e145531246c9c7f15872a76
Maco <hellqvistmarcus@gmail.com>
2021-05-17 10:03:36 +0200

    det ska vara frågetecken

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Grupp 3 new!!!!" />
+    <HelloWorld msg="Welcome to Your Grupp 3 new????" />
   </div>
 </template>
 
