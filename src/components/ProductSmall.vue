414393eb580244d336f9aa887bd0baf7aed84a92
Maco <hellqvistmarcus@gmail.com>
2021-05-18 11:58:01 +0200

    add ProductSmall component

---
+++ src/components/ProductSmall.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-card
+      :title="name"
+      :img-src="productImage"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{ description }}
+      </b-card-text>
+
+      <b-card-text> {{ price }} SEK </b-card-text>
+      <b-link :to="{ name: 'Product', params: { productId: productId } }"
+        >Details</b-link
+      >
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productId: String,
+    name: String,
+    description: String,
+    price: Number,
+    productImage: String,
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <h1>Start page - Home</h1>
     <p>{{ $route.params.categoryId }}</p>
+    <product-small
+      productId="id123"
+      name="product-title"
+      description="product-text"
+      :price="999"
+      productImage="urlToImage"
+    ></product-small>
     <div v-if="this.$store.state.products">
       <ol>
         <li v-for="product in this.$store.state.products" :key="product.id">
@@ -19,49 +26,47 @@
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        product: null,
-        filteredArray: []
-      }
-    },
+import ProductSmall from "../components/ProductSmall.vue";
+export default {
+  components: { ProductSmall },
+  name: "Home",
+  data() {
+    return {
+      product: null,
+      filteredArray: [],
+    };
+  },
 
-    created() {
-      this.fetchLocalData()
+  created() {
+    this.fetchLocalData();
+  },
+  methods: {
+    fetchLocalData() {
+      fetch("products.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.$store.commit("fillWithProducts", data.products);
+        });
     },
-    methods: {
-      fetchLocalData() {
-        fetch('products.json')
-          .then((response) => response.json())
-          .then((data) => {
-            this.$store.commit('fillWithProducts', data.products)
-          })
-      },
-      filterArray() {
-        for (
-          let index = 0;
-          index < this.$store.state.products.length;
-          index++
+    filterArray() {
+      for (let index = 0; index < this.$store.state.products.length; index++) {
+        if (
+          JSON.stringify(this.$store.state.products[index].category) ===
+          this.$route.params.categoryId
         ) {
-          if (
-            JSON.stringify(this.$store.state.products[index].category) ===
-            this.$route.params.categoryId
-          ) {
-            this.filteredArray.push(this.$store.state.products[index])
-          }
+          this.filteredArray.push(this.$store.state.products[index]);
         }
       }
     },
-    watch: {
-      $route(to, from) {
-        if (to !== from) {
-          this.filteredArray = []
-          console.log('Changed route to: ' + to.params.categoryId)
-          this.filterArray()
-        }
+  },
+  watch: {
+    $route(to, from) {
+      if (to !== from) {
+        this.filteredArray = [];
+        console.log("Changed route to: " + to.params.categoryId);
+        this.filterArray();
       }
-    }
-  }
+    },
+  },
+};
 </script>
