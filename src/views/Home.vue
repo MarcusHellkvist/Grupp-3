843c36fc756cb64392be14e76256c034026614ac
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-05-18 12:08:39 +0200

    min dotters bil: kia ceed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,8 @@
           tag="article"
           style="max-width: 20rem;"
           class="mb-2"
-          v-for="product in this.$store.state.products" :key="product.id"
+          v-for="product in this.$store.state.products"
+          :key="product.id"
           no-gutters
         >
           <b-card-text>
@@ -51,68 +52,56 @@
           aria-controls="my-table"
         ></b-pagination>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
+  import ProductSmall from '../components/ProductSmall.vue'
   export default {
+    components: { ProductSmall },
     name: 'Home',
     data() {
       return {
         product: null,
-        filteredArray: [],
-        perPage: 5,
-        currentPage: 1,
-        totalRows: this.$store.state.products.length
+        filteredArray: []
       }
     },
-    props: {},
-=======
-import ProductSmall from "../components/ProductSmall.vue";
-export default {
-  components: { ProductSmall },
-  name: "Home",
-  data() {
-    return {
-      product: null,
-      filteredArray: [],
-    };
-  },
->>>>>>> 414393eb580244d336f9aa887bd0baf7aed84a92
 
-  created() {
-    this.fetchLocalData();
-  },
-  methods: {
-    fetchLocalData() {
-      fetch("products.json")
-        .then((response) => response.json())
-        .then((data) => {
-          this.$store.commit("fillWithProducts", data.products);
-        });
+    created() {
+      this.fetchLocalData()
     },
-    filterArray() {
-      for (let index = 0; index < this.$store.state.products.length; index++) {
-        if (
-          JSON.stringify(this.$store.state.products[index].category) ===
-          this.$route.params.categoryId
+    methods: {
+      fetchLocalData() {
+        fetch('products.json')
+          .then((response) => response.json())
+          .then((data) => {
+            this.$store.commit('fillWithProducts', data.products)
+          })
+      },
+      filterArray() {
+        for (
+          let index = 0;
+          index < this.$store.state.products.length;
+          index++
         ) {
-          this.filteredArray.push(this.$store.state.products[index]);
+          if (
+            JSON.stringify(this.$store.state.products[index].category) ===
+            this.$route.params.categoryId
+          ) {
+            this.filteredArray.push(this.$store.state.products[index])
+          }
         }
       }
     },
-  },
-  watch: {
-    $route(to, from) {
-      if (to !== from) {
-        this.filteredArray = [];
-        console.log("Changed route to: " + to.params.categoryId);
-        this.filterArray();
+    watch: {
+      $route(to, from) {
+        if (to !== from) {
+          this.filteredArray = []
+          console.log('Changed route to: ' + to.params.categoryId)
+          this.filterArray()
+        }
       }
-    },
-  },
-};
+    }
+  }
 </script>
