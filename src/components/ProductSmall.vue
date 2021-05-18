c26d2a4396804b6075366f19f49712952860e2e5
Maco <hellqvistmarcus@gmail.com>
2021-05-18 12:48:20 +0200

    add buy button and addToCart mutation

--- src/components/ProductSmall.vue
+++ src/components/ProductSmall.vue
@@ -17,18 +17,36 @@
       <b-link :to="{ name: 'Product', params: { productId: productId } }"
         >Details</b-link
       >
+      <b-button @click="buyProduct">Buy</b-button>
     </b-card>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      product: {
+        productId: this.productId,
+        name: this.name,
+        description: this.description,
+        price: this.price,
+        productImage: this.productImage,
+      },
+    };
+  },
   props: {
     productId: String,
     name: String,
     description: String,
     price: Number,
     productImage: String,
+  },
+  methods: {
+    buyProduct() {
+      console.log("you bought it!");
+      this.$store.commit("addToCart", this.product);
+    },
   },
 };
 </script>
