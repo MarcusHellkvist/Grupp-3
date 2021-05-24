e9cb33d37475aabd60da534f00f606a669821d03
Maco <hellqvistmarcus@gmail.com>
2021-05-24 13:47:30 +0200

    add product-small styling and custom-event

--- src/components/ProductSmall.vue
+++ src/components/ProductSmall.vue
@@ -1,54 +1,77 @@
 <template>
-  <div>
-    <b-card
-      :title="name"
-      :img-src="productImage"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-      <b-card-text>
-        {{ description }}
-      </b-card-text>
+  <b-card
+    no-body
+    :img-src="book.image"
+    img-alt="book image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text class="title">
+      <b-link :to="{ name: 'Product', params: { productId: book.isbn } }">{{
+        book.title
+      }}</b-link>
+    </b-card-text>
+    <b-card-text class="author">
+      {{ book.author }}
+    </b-card-text>
 
-      <b-card-text> {{ price }} SEK </b-card-text>
-      <b-link :to="{ name: 'Product', params: { productId: productId } }"
-        >Details</b-link
-      >
-      <b-button @click="buyProduct">Buy</b-button>
-    </b-card>
-  </div>
+    <b-button @click="buyProduct" size="sm" variant="primary">
+      $ {{ book.price }}
+      <b-icon icon="cart" aria-hidden="true"></b-icon>
+    </b-button>
+  </b-card>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      product: {
-        productId: this.productId,
-        name: this.name,
-        description: this.description,
-        price: this.price,
-        productImage: this.productImage,
+      book: {
+        isbn: "this.isbn",
+        title: "this.title",
+        author: "this.author",
+        price: "this.price",
+        image:
+          "https://s2.adlibris.com/images/57965498/burn-after-writing-pink.jpg",
       },
     };
   },
   props: {
-    productId: String,
-    name: String,
-    description: String,
+    isbn: String,
+    title: String,
+    author: String,
     price: Number,
-    productImage: String,
+    image: String,
   },
   methods: {
     buyProduct() {
       console.log("you bought it!");
-      this.$store.commit("addToCart", this.product);
+      this.$store.commit("addToCart", this.book);
+      this.$emit("book-to-cart-alert", this.book);
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+p.card-text.title {
+  font-size: 18px;
+  font-weight: bold;
+  text-align: start;
+  margin: 6px 12px 0px;
+}
+p.card-text.author {
+  font-size: 16px;
+  text-align: start;
+  margin: 0px 12px;
+}
+button {
+  margin: 24px 12px 12px;
+  font-size: 18px;
+}
+img {
+  padding: 12px 12px 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,7 @@
       <div class="row">
         <div class="col" v-for="product in allProducts" :key="product.id">
           <product-small
+            @book-to-cart-alert="onBookToCartToast"
             :productId="product.id"
             :name="product.name"
             :description="product.description"
@@ -41,44 +42,54 @@
 </template>
 
 <script>
-  import ProductSmall from '../components/ProductSmall.vue'
-  export default {
-    components: { ProductSmall },
-    name: 'Home',
-    data() {
-      return {
-        products: this.$store.state.products, // all product "use to sort Price"
-        perPage: 4,
-        currentPage: 1,
-        defaultImage: this.$store.state.defaultImage
-      }
-    },
-    computed: {
-      rows() {
-        return this.$store.state.products.length
-      },
-
-      allProducts() {
-        return this.$store.state.products.slice(
-          (this.currentPage - 1) * this.perPage,
-          this.currentPage * this.perPage
-        )
-      }
+import ProductSmall from "../components/ProductSmall.vue";
+export default {
+  components: { ProductSmall },
+  name: "Home",
+  data() {
+    return {
+      products: this.$store.state.products, // all product "use to sort Price"
+      perPage: 4,
+      currentPage: 1,
+      defaultImage: this.$store.state.defaultImage,
+    };
+  },
+  computed: {
+    rows() {
+      return this.$store.state.products.length;
     },
 
-    methods: {
-      maxPrice() {
-        this.products.sort(function(a, b) {
-          return b.price - a.price
-        })
-        console.log(this.products)
-      },
-      minPrice() {
-        this.products.sort(function(a, b) {
-          return a.price - b.price
-        })
-        console.log(this.products)
-      }
-    }
-  }
+    allProducts() {
+      return this.$store.state.products.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage
+      );
+    },
+  },
+
+  methods: {
+    maxPrice() {
+      this.products.sort(function(a, b) {
+        return b.price - a.price;
+      });
+      console.log(this.products);
+    },
+    minPrice() {
+      this.products.sort(function(a, b) {
+        return a.price - b.price;
+      });
+      console.log(this.products);
+    },
+    onBookToCartToast(book) {
+      this.$bvToast.toast(`${book.title} was added to your cart`, {
+        title: "Success",
+        autoHideDelay: 2000,
+        appendToast: true,
+        variant: "success",
+        solid: true,
+        toaster: "b-toaster-top-center",
+      });
+    },
+  },
+};
 </script>
