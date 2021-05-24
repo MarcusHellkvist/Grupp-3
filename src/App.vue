8f3a6928bd13c520dae320a75f146b2bfb3a7c21
Maco <hellqvistmarcus@gmail.com>
2021-05-24 16:17:42 +0200

    add footer and news-letter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container>
+  <b-container fluid>
     <div id="app">
       <b-row id="nav">
         <b-col>
@@ -58,6 +58,54 @@
         </b-col>
       </b-row>
     </div>
+    <!-- footer -->
+    <b-row class="news-letter">
+      <b-col cols="12">
+        <b-form inline>
+          <span
+            >Learn about new offers and get more deals by joining our
+            newsletter</span
+          >
+          <b-form-input
+            class="mr-2 ml-2"
+            size="sm"
+            placeholder="Email Address"
+          ></b-form-input>
+          <b-button size="sm" variant="primary">Sign up now</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+    <b-row class="footer">
+      <b-col md="3" sm="12">
+        <h6>Explore</h6>
+        <ul>
+          <li><router-link to="#">About us</router-link></li>
+          <li><router-link to="#">Help</router-link></li>
+          <li><router-link to="#">Press</router-link></li>
+          <li><router-link to="#">Stores</router-link></li>
+        </ul>
+      </b-col>
+      <b-col md="3" sm="12">
+        <h6>Account</h6>
+        <ul>
+          <li><router-link to="#">Orders</router-link></li>
+          <li><router-link to="#">Wishlist</router-link></li>
+          <li><router-link to="#">Customer information</router-link></li>
+        </ul>
+      </b-col>
+      <b-col md="3" sm="12">
+        <h6>Important Stuff</h6>
+        <ul>
+          <li><router-link to="#">Customer support</router-link></li>
+          <li><router-link to="#">Cookies</router-link></li>
+          <li><router-link to="#">FAQ</router-link></li>
+          <li><router-link to="#">Terms &amp; conditions</router-link></li>
+        </ul>
+      </b-col>
+      <b-col md="3" sm="12">
+        <h6>We accept these payment methods</h6>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -123,5 +171,32 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.footer {
+  background: #403042;
+  padding: 20px;
+}
+
+.footer a {
+  color: white;
+}
+
+.footer ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.footer li {
+  padding: 8px;
+  border-bottom: 1px solid rgb(255, 255, 255, 0.1);
+}
+
+.news-letter {
+  background-color: white;
+  margin-top: 24px;
+  padding: 12px;
+  color: black;
 }
 </style>
