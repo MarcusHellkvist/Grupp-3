1c27c61b1a01ed81c7cb9019f3fb39f2e50f07de
atlalbasha <atlal.basha@gmail.com>
2021-05-26 10:20:11 +0200

    topten rating

--- src/components/TopTenBooks.vue
+++ src/components/TopTenBooks.vue
@@ -3,24 +3,33 @@
     <div>
       <b-list-group>
         <h4 class="text-left">Top 10</h4>
+
         <b-list-group-item
-          v-for="(product, index) in top10.slice(0, 10)"
+          v-for="(product, index) in topTen"
           :key="index"
           class="d-flex justify-content-between"
-          variant="light"
-          href="#"
+          :to="{ name: 'Product', params: { isbn: product.isbn } }"
         >
           <div class="text-left">
             <h6>
               <span class="top-ten">{{ index + 1 }}.</span> {{ product.title }}
             </h6>
+
             <small>{{ product.author }}</small>
+
             <p>Price: ${{ product.price }}</p>
+            <b-rating
+              inline
+              :value="product.rating"
+              size="sm"
+              readonly
+              no-border
+            ></b-rating>
           </div>
 
           <div>
             <div>
-              <img :src="product.image" width="50" height="75" />
+              <img :src="product.image" width="50" height="80" />
             </div>
           </div>
         </b-list-group-item>
@@ -33,8 +42,24 @@
 export default {
   data() {
     return {
-      top10: this.$store.state.books,
+      books: this.$store.state.books,
     };
+  },
+  created() {
+    this.topTenRating();
+  },
+  computed: {
+    topTen() {
+      return this.$store.state.books.slice(0, 10);
+    },
+  },
+  methods: {
+    topTenRating() {
+      this.books.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      console.log(this.books);
+    },
   },
 };
 </script>
@@ -53,5 +78,10 @@ small {
 }
 .top-ten {
   color: red;
+}
+b-rating {
+  padding: 0rem 0rem;
+  margin: 0rem;
+  text-align: right;
 }
 </style>
