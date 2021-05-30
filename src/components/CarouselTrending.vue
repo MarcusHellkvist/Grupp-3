21e28913a74a4ad8d4bc909e6418a81dc5ca26c1
Maco <hellqvistmarcus@gmail.com>
2021-05-30 21:37:32 +0200

    cleaned up homepage

--- src/components/CarouselTrending.vue
+++ src/components/CarouselTrending.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row>
-    <b-col style="border: 2px solid">
-      <h2>Trending</h2>
+    <b-col>
+      <h3>{{ title }}</h3>
       <b-carousel
         id="carousel-1"
         v-model="slide"
@@ -14,10 +14,10 @@
         <!-- Slide with cards -->
         <b-carousel-slide img-blank>
           <div class="row">
-            <div class="">
+            <div class="col-12 sm-6">
               <b-card-group deck>
                 <product-small
-                  v-for="book in $store.state.trending.slice(0, 4)"
+                  v-for="book in this.booksInCarousel.slice(0, 4)"
                   :key="book.isbn"
                   :isbn="book.isbn"
                   :title="book.title"
@@ -34,7 +34,7 @@
             <div class="">
               <b-card-group deck>
                 <product-small
-                  v-for="book in $store.state.trending.slice(4, 8)"
+                  v-for="book in this.booksInCarousel.slice(4, 8)"
                   :key="book.isbn"
                   :isbn="book.isbn"
                   :title="book.title"
@@ -52,66 +52,70 @@
 </template>
 
 <script>
-  import ProductSmall from '../components/ProductSmall.vue'
-  export default {
-    components: { ProductSmall },
-    name: 'Trending',
-    data() {
-      return {
-        slide: 0,
-        sliding: null,
-        defaultImage: this.$store.state.defaultImage,
-        sliceStartValue: 0,
-        sliceEndValue: 4,
-        book: this.$store.state.books
+import ProductSmall from "../components/ProductSmall.vue";
+export default {
+  components: { ProductSmall },
+  name: "Trending",
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+      defaultImage: this.$store.state.defaultImage,
+      sliceStartValue: 0,
+      sliceEndValue: 4,
+      book: this.$store.state.books,
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliceStartValue += 4;
+      this.sliceEndValue += 4;
+      this.sliding = true;
+      if (this.sliceStartValue > 4) {
+        this.sliceStartValue = 0;
+        this.sliceEndValue = 4;
       }
     },
-    methods: {
-      onSlideStart() {
-        this.sliceStartValue += 4
-        this.sliceEndValue += 4
-        this.sliding = true
-        if (this.sliceStartValue > 4) {
-          this.sliceStartValue = 0
-          this.sliceEndValue = 4
-        }
-      },
-      onSlideEnd() {
-        this.sliding = false
-      }
+    onSlideEnd() {
+      this.sliding = false;
     },
-    computed: {
-      showProducts() {
-        return this.$store.state.books.slice(
-          this.sliceStartValue,
-          this.sliceEndValue
-        )
-      }
-    }
-  }
+  },
+  computed: {
+    showProducts() {
+      return this.$store.state.books.slice(
+        this.sliceStartValue,
+        this.sliceEndValue
+      );
+    },
+  },
+  props: {
+    booksInCarousel: [],
+    title: String,
+  },
+};
 </script>
 
 <style scoped>
-  .carousel >>> .carousel-item {
-    height: 375px;
-  }
-  .carousel >>> .carousel-indicators li {
-    background-color: #403042;
-  }
-  .carousel >>> .carousel-control-prev-icon {
-    height: 40px;
-    width: 40px;
-    outline: black;
-    background-size: 35%, 35%;
-    border-radius: 50%;
-    background-color: #403042;
-  }
-  .carousel >>> .carousel-control-next-icon {
-    height: 40px;
-    width: 40px;
-    outline: black;
-    background-size: 35%, 35%;
-    border-radius: 50%;
-    background-color: #403042;
-  }
+.carousel >>> .carousel-item {
+  height: 375px;
+}
+.carousel >>> .carousel-indicators li {
+  background-color: #403042;
+}
+.carousel >>> .carousel-control-prev-icon {
+  height: 40px;
+  width: 40px;
+  outline: black;
+  background-size: 35%, 35%;
+  border-radius: 50%;
+  background-color: #403042;
+}
+.carousel >>> .carousel-control-next-icon {
+  height: 40px;
+  width: 40px;
+  outline: black;
+  background-size: 35%, 35%;
+  border-radius: 50%;
+  background-color: #403042;
+}
 </style>
