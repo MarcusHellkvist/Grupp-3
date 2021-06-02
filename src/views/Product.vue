8554c4849bd7f8ae3399cf5632dd22c377585eae
alonakirichenkoivanovna <alena1412199217@gmail.com>
2021-06-02 14:58:49 +0200

    fixed shopping cart with firebase

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,210 +1,9 @@
 <template>
-  <div>
-    <!-- FRIDAY -->
-    <!-- BAR -->
-    <!-- <b-container class="first">
-      <b-row class="text-center">
-        <b-col id="nameForSite">NAME FOR SITE HAHA</b-col>
-      </b-row>
-      <b-row class="bv-example-row bv-example-row-flex-cols">
-        <b-col id="category1" align-self="baseline"
-          ><router-link name="cat1" to="/category/1"
-            >Category 1</router-link
-          ></b-col
-        >
-        <b-col id="category2" align-self="baseline"
-          ><router-link to="/category/2">Category 2</router-link></b-col
-        >
-        <b-col id="category3" align-self="baseline"
-          ><router-link to="/category/3">Category 3</router-link></b-col
-        >
-        <b-col id="shopping-cart" align-self="stretch"
-          ><router-link to="/cart"
-            ><ShoppingCartButton></ShoppingCartButton></router-link
-        ></b-col>
-      </b-row>
-
-      <b-row id="search" class="mr-2 text-center ">
-        <b-col
-          ><b-input-group size="md" class=" mb-2">
-            <b-input-group-prepend is-text>
-              <b-icon icon="search" variant="dark"></b-icon>
-            </b-input-group-prepend>
-            <vue-typeahead-bootstrap
-              :data="productNames"
-              v-model="searchQuery"
-              placeholder="Search Product"
-            /> </b-input-group
-        ></b-col>
-      </b-row>
-    </b-container> -->
-    <!-- BAR -->
-
-    <!--OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  -->
-    <!-- EVENING -->
-    <!--  <b-contaoner fluid="md">
-      <b-row id="main">
-        <b-col align-self="stretch" cols="4">
-          <img
-            src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
-            alt=""
-            width="300px"
-            height="400px"
-          />
-        </b-col>
-        <b-row align-h="right">
-          <b-col>NAME</b-col>
-          <b-col>Author: {{ authorName }}</b-col>
-        </b-row>
-
-        <b-col align-h="center" cols="12">
-          <b-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-            odit dignissimos sequi aspernatur, sapiente eveniet nostrum
-            laboriosam, obcaecati nesciunt vitae quidem voluptatum corporis
-            maiores autem hic incidunt alias repellendus! Autem?
-            {{ productDescription }}
-          </b-text>
-        </b-col>
-
-        <b-col
-          ><div id="price" class="text-left ">
-            Price: {{ productPrice }} SEK
-          </div>
-        </b-col>
-        <b-col cols="6"
-          ><div class=" text-right">
-            <b-button id="addToShopingCart" @click="onClick"
-              >ADD TO CART
-              <b-icon
-                id="icon-cart-check"
-                style="color: white; "
-                icon="cart4"
-                class=" position-relative"
-                font-scale="2"
-              ></b-icon>
-            </b-button>
-          </div>
-        </b-col>
-      </b-row>
-    </b-contaoner> -->
-    <!-- EVENING -->
-    <!--  -->
-
-    <!-- FRIDAY -->
-
-    <!-- <p>{{ productID }}</p>
-    <ShoppingCartButton
-      :numberOfItems="this.$store.state.cart.length"
-    ></ShoppingCartButton> -->
-
-    <!-- <ShoppingCartButton></ShoppingCartButton> -->
-
-    <div d-flex>
-      <!--  style="width: 800px; " -->
-      <!-- <b-card
-        :title="productName"
-        :img-src="productPhoto"
-        img-alt="Image"
-        img-left
-        class="mb-3"
-        tag="article"
-      > -->
-
-      <!-- ALONA MONDAY -->
-      <!-- <b-container>
-        <b-row>
-          <b-col cols="4">
-            <b-card
-              title="productName"
-              img-src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80"
-              img-alt="Card image"
-              img-left
-              img-width="500px"
-              text-variant="left"
-              class="mw-100"
-            >
-              <b-row>
-                <b-col cols="12">
-                  <b-card-text> Author: {{ authorName }} </b-card-text>
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col cols="6"
-                  ><b-card-text id="price" class="text-left ">
-                    Price: {{ productPrice }} SEK
-                  </b-card-text></b-col
-                >
-                <b-col cols="6"
-                  ><div class=" text-right">
-                    <b-button id="addToShopingCart" @click="onClick"
-                      >ADD TO CART
-                      <b-icon
-                        id="icon-cart-check"
-                        style="color: white; "
-                        icon="cart4"
-                        class=" position-relative"
-                        font-scale="2"
-                      ></b-icon>
-                    </b-button>
-                  </div>
-                </b-col>
-              </b-row>
-              <hr />
-
-              <b-card-text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloremque odit dignissimos sequi aspernatur, sapiente eveniet
-                nostrum laboriosam, obcaecati nesciunt vitae quidem voluptatum
-                corporis maiores autem hic incidunt alias repellendus! Autem?
-                {{ productDescription }}
-              </b-card-text>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-container> -->
-      <!-- ALONA MONDAY -->
-    </div>
-
-    <!-- <b-container>
-      <b-row>
-        <b-col cols="12" sm="6" style="background-color: pink;">
-          1
-          <b-row>
-            <b-col sm="6" style="background-color: red;"> 2 </b-col>
-          </b-row>
-        </b-col>
-
-        <b-col cols="12" sm="6">
-          <b-row class="ml-2">
-            <b-col cols="6" sm="12" style="background-color: black;"> 2 </b-col>
-            <b-col
-              class="ml-1"
-              cols="6"
-              sm="12"
-              style="background-color: blue;"
-            >
-              3</b-col
-            >
-
-            <b-col md="6" style="background-color: yellow;"> 4</b-col>
-            <b-col cols="6" md="6" style="background-color: green;"> 5</b-col>
-
-            <b-col style="background-color: #51355A">
-              3.1
-            </b-col>
-            <b-col style="background-color: #BA6E6E"> 3.2</b-col>
-          </b-row>
-        </b-col>
-      </b-row>
-max-height:150px
-      <b-row class="d-flex flex-row-reverse bd-highlight"> </b-row>
-    </b-container> -->
-
+  <div v-if="book">
     <b-container class="mt-4">
       <b-row no-gutters style="background-color: white;  " class=" pr-2">
         <b-col cols="12" md="3" class="" style=" ">
-          <img style="width: 95%;   " :src="image" alt="BOOK PHOTO" />
+          <img style="width: 95%;   " :src="book.image" alt="BOOK PHOTO" />
         </b-col>
         <b-col>
           <b-row>
@@ -214,11 +13,11 @@ max-height:150px
               md="12"
               style="text-align: left; font-size: 180%; letter-spacing: -1px;  font-weight: bold;
             "
-              >{{ title }}
+              >{{ book.title }}
             </b-col>
 
             <b-col id="authorStyle" class="ml-4 mb-4" cols="12" md="12">
-              Author: {{ author }}
+              Author: {{ book.author }}
               <!-- <hr
             /> --></b-col
             >
@@ -228,7 +27,7 @@ max-height:150px
               style="font-weight: bold; font-size: 180%; letter-spacing: -1px;  font-weight: bold; text-align: left; "
               cols="6"
               md="6"
-              >Price: {{ price }}$
+              >Price: {{ book.price }}$
             </b-col>
             <b-col class="" cols="5" md="5"
               ><div class=" text-right">
@@ -285,8 +84,18 @@ max-height:150px
           </b-col>
 
           <b-col id="plotStyle" class="mb-4" cols="12" md="12">
-            {{ plot }}</b-col
+            {{ book.plot }}</b-col
           >
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+      <b-row cols="12" md="3">
+        <b-col class="mt-4">
+          <carousel-of-interest
+            :booksInCarousel="this.$store.state.books.slice(0, 8)"
+            title="New books"
+          ></carousel-of-interest>
         </b-col>
       </b-row>
     </b-container>
@@ -294,29 +103,15 @@ max-height:150px
 </template>
 
 <script>
-  /* import ShoppingCartButton from '../components/ShoppingCartButton.vue' */
+  import * as firebase from '../firebase.js'
+
+  import CarouselOfInterest from '../components/CarouselOfInterest.vue'
   export default {
-    /*  components: { ShoppingCartButton },
-    name: 'Product', */
+    components: { CarouselOfInterest },
+    name: 'Product',
     data() {
       return {
-        author: '',
-        image: '',
-        plot: '',
-        price: 0,
-        genre: '',
-        pages: 0,
-        publisher: '',
-        stock: 0,
-        rating: 0,
-        language: '',
-        title: '',
-        published: '',
-
-        isbn: this.$route.params.isbn,
-        /* productsArr: this.$store.state.books, */
-        productsArr: [],
-        oneObject: {},
+        book: null,
         quantityinCart: 0
       }
     },
@@ -326,58 +121,46 @@ max-height:150px
       }
     }, */
     created() {
-      this.fetchLocalData()
-
-      console.log('CREATED LOOK FOR ARRAY', this.productsArr)
-      /* this.fetchLocalData() */
-      /*  this.numberOfItemsInCart = this.$store.state.cart.length */
+      this.getBook()
     },
     methods: {
-      fetchLocalData() {
-        fetch('books.json')
-          .then((response) => response.json())
-          .then((data) => {
-            this.productsArr = data.books
+      getBook() {
+        var docRef = firebase.booksCollection.doc(this.$route.params.isbn)
 
-            console.log('hi ffffffff', this.productsArr)
-
-            console.log('METHOD', this.isbn)
-            this.pickRightObject()
-            this.giveVAlueToObject()
+        docRef
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              console.log('Document data:', doc.data())
+              this.book = doc.data()
+            } else {
+              // doc.data() will be undefined in this case
+              console.log('No such document!')
+            }
+          })
+          .catch((error) => {
+            console.log('Error getting document:', error)
           })
       },
-      pickRightObject() {
-        /* this.productsArr = this.rows */
-        /* this.productsArr = this.$store.state.books */
 
-        for (let i = 0; i < this.productsArr.length; i++) {
-          if (this.productsArr[i].isbn === this.isbn) {
-            /* this.oneObject = this.productsArr[i] */
-
-            this.oneObject = {
-              isbn: this.productsArr[i].isbn,
-              title: this.productsArr[i].title,
-              author: this.productsArr[i].author,
-              plot: this.productsArr[i].plot,
-              price: this.productsArr[i].price,
-              image: this.productsArr[i].image
-            }
-
-            console.log('MY OBJECT', this.oneObject)
-          }
-        }
-        return this.oneObject
-      },
-      giveVAlueToObject() {
-        this.title = this.oneObject.title
-        this.image = this.oneObject.image
-        /* this.isbn = this.oneObject.isbn */
-        this.price = this.oneObject.price
-        this.author = this.oneObject.author
-        this.plot = this.oneObject.plot
-      },
       onClick() {
-        this.$store.commit('addToCart', this.oneObject)
+        if (this.$store.state.user.loggedIn === true) {
+          firebase.usersCollection
+            .doc(this.$store.state.user.data.uid)
+            .collection('cart')
+            .doc(this.book.isbn)
+            .set(this.book)
+            .then(() => {
+              console.log('Document successfully written!')
+            })
+            .catch((error) => {
+              console.error('Error writing document: ', error)
+            })
+          console.log('User is true')
+        } else {
+          console.log('User is false')
+        }
+        /* this.$store.commit('addToCart', this.book) */
         /* this.$store.commit('quantityInCart') */
 
         /* this.$store.state.cart.length++ */
@@ -387,13 +170,7 @@ max-height:150px
     watch: {
       '$route.params.isbn'(to, from) {
         console.log(`params changed - to: ${to} from:${from}`)
-
-        this.isbn = to
-        this.fetchLocalData()
-
-        console.log('IN WATCH ', this.isbn)
-
-        // Fixa så det fungerar i realtid, just nu måste man uppdatera hemsidan manuellt för att ändringar ska ske.
+        this.getBook()
       }
     }
   }
