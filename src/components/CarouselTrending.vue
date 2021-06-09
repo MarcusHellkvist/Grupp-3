27a1e1b77e35818efaef4a8f5f50fbeb14fab171
atlalbasha <atlal.basha@gmail.com>
2021-06-09 10:43:30 +0200

    add height to carousell card

--- src/components/CarouselTrending.vue
+++ src/components/CarouselTrending.vue
@@ -15,7 +15,7 @@
         <b-carousel-slide img-blank>
           <div class="row">
             <div class="col-12 sm-6">
-              <b-card-group deck>
+              <b-card-group deck style="height:20rem">
                 <product-small
                   v-for="book in this.booksInCarousel.slice(0, 4)"
                   :key="book.isbn"
@@ -32,7 +32,7 @@
         <b-carousel-slide img-blank>
           <div class="row">
             <div class="">
-              <b-card-group deck>
+              <b-card-group deck style="height:20rem">
                 <product-small
                   v-for="book in this.booksInCarousel.slice(4, 8)"
                   :key="book.isbn"
@@ -52,70 +52,70 @@
 </template>
 
 <script>
-import ProductSmall from "../components/ProductSmall.vue";
-export default {
-  components: { ProductSmall },
-  name: "Trending",
-  data() {
-    return {
-      slide: 0,
-      sliding: null,
-      defaultImage: this.$store.state.defaultImage,
-      sliceStartValue: 0,
-      sliceEndValue: 4,
-      book: this.$store.state.books,
-    };
-  },
-  methods: {
-    onSlideStart() {
-      this.sliceStartValue += 4;
-      this.sliceEndValue += 4;
-      this.sliding = true;
-      if (this.sliceStartValue > 4) {
-        this.sliceStartValue = 0;
-        this.sliceEndValue = 4;
+  import ProductSmall from '../components/ProductSmall.vue'
+  export default {
+    components: { ProductSmall },
+    name: 'Trending',
+    data() {
+      return {
+        slide: 0,
+        sliding: null,
+        defaultImage: this.$store.state.defaultImage,
+        sliceStartValue: 0,
+        sliceEndValue: 4,
+        book: this.$store.state.books
       }
     },
-    onSlideEnd() {
-      this.sliding = false;
+    methods: {
+      onSlideStart() {
+        this.sliceStartValue += 4
+        this.sliceEndValue += 4
+        this.sliding = true
+        if (this.sliceStartValue > 4) {
+          this.sliceStartValue = 0
+          this.sliceEndValue = 4
+        }
+      },
+      onSlideEnd() {
+        this.sliding = false
+      }
     },
-  },
-  computed: {
-    showProducts() {
-      return this.$store.state.books.slice(
-        this.sliceStartValue,
-        this.sliceEndValue
-      );
+    computed: {
+      showProducts() {
+        return this.$store.state.books.slice(
+          this.sliceStartValue,
+          this.sliceEndValue
+        )
+      }
     },
-  },
-  props: {
-    booksInCarousel: Array,
-    title: String,
-  },
-};
+    props: {
+      booksInCarousel: Array,
+      title: String
+    }
+  }
 </script>
 
 <style scoped>
-.carousel >>> .carousel-item {
-  height: 375px;
-}
-.carousel >>> .carousel-indicators li {
-  background-color: #403042;
-}
-.carousel >>> .carousel-control-prev-icon {
-  height: 40px;
-  width: 40px;
-  outline: black;
-  background-size: 35%, 35%;
-  border-radius: 50%;
-  background-color: #403042;
-}
-.carousel >>> .carousel-control-next-icon {
-  height: 40px;
-  width: 40px;
-  outline: black;
-  background-size: 35%, 35%;
-  border-radius: 50%;
-  background-color: #403042;
-}
+  .carousel >>> .carousel-item {
+    height: 375px;
+  }
+  .carousel >>> .carousel-indicators li {
+    background-color: #403042;
+  }
+  .carousel >>> .carousel-control-prev-icon {
+    height: 40px;
+    width: 40px;
+    outline: black;
+    background-size: 35%, 35%;
+    border-radius: 50%;
+    background-color: #403042;
+  }
+  .carousel >>> .carousel-control-next-icon {
+    height: 40px;
+    width: 40px;
+    outline: black;
+    background-size: 35%, 35%;
+    border-radius: 50%;
+    background-color: #403042;
+  }
 </style>
